b0f3360d2853dec97fd45a0c703197c3e626a232
shRahat10 <shahriar.haque.1011@gmail.com>
2024-03-27 15:47:44 +0600

    author

--- src/components/Author Details/AuthorDetails.jsx
+++ src/components/Author Details/AuthorDetails.jsx
@@ -2,8 +2,12 @@ const AuthorDetails = ({ book }) => {
     const { author, authorImage, authorBio } = book;
     
     return (
-        <div>
-            <figure className=" w-80 h-80 mt-5"><img src={authorImage} alt="" /></figure>
+        <div className=" border rounded-2xl p-4 mb-4 grid lg:grid-cols-4 gap-5">
+            <figure className=" rounded-2xl"><img className="rounded-2xl mx-auto " src={authorImage} alt="" /></figure>
+            <div className=" col-span-3">
+                <h1 className=" font-bold text-xl">{author}</h1>
+                <p><span className=" font-bold text-lg">Bio:</span> {authorBio}</p>
+            </div>
         </div>
     );
 };
--- src/components/Authors/Authors.jsx
+++ src/components/Authors/Authors.jsx
@@ -6,7 +6,7 @@ const Authors = () => {
 
     return (
         <div>
-            <h1 className="text-4xl text-center font-bold">Authors</h1>
+            <h1 className="text-4xl text-center font-bold mb-5">Authors</h1>
             {
                 books.map((book, idx) => <AuthorDetails key={idx} book={book}></AuthorDetails>)
             }
