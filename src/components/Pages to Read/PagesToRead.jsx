23ea50d5214237c6dcd6eee5c3de5f722337f726
shRahat10 <shahriar.haque.1011@gmail.com>
2024-03-27 10:17:27 +0600

    chart

--- src/components/Pages to Read/PagesToRead.jsx
+++ src/components/Pages to Read/PagesToRead.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { getList } from "../../utility/localStorage";
-// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const PageToRead = () => {
     const books = useLoaderData();
@@ -12,44 +12,44 @@ const PageToRead = () => {
             totalPages: book.totalPages,
         };
     });
-    
-    // const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-    // const getPath = (x, y, width, height) => {
-    //     return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
-    //     ${x + width / 2}, ${y}
-    //     C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-    //     Z`;
-    // };
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-    // const TriangleBar = (props) => {
-    //     const { fill, x, y, width, height } = props;
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
 
-    //     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-    // };
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
 
     return (
         <div>
-            {/* <BarChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 20,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="bookName" />
-                <YAxis />
-                <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-                    ))}
-                </Bar>
-            </BarChart> */}
+            <ResponsiveContainer width='100%' height={500}>
+                <BarChart
+                    data={data}
+                    margin={{
+                        top: 50,
+                        right: 10,
+                        left: 10,
+                        bottom: 50,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="bookName" interval={0} angle={-65} textAnchor="end" tick={{ fontSize: 12 }} height={100} tickFormatter={(value) => value.split(' ').map((word, index) => index % 2 === 0 ? word : '\n' + word)} />
+                    <YAxis />
+                    <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                        ))}
+                    </Bar>
+                </BarChart>
+            </ResponsiveContainer >
         </div>
     );
 };
