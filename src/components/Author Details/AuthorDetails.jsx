ace316642384f23dc0eff9ae3fbf6c7cd510e33d
shRahat10 <shahriar.haque.1011@gmail.com>
2024-03-27 16:26:11 +0600

    author correction

--- src/components/Author Details/AuthorDetails.jsx
+++ src/components/Author Details/AuthorDetails.jsx
@@ -3,7 +3,7 @@ const AuthorDetails = ({ book }) => {
     
     return (
         <div className=" border rounded-2xl p-4 mb-4 grid lg:grid-cols-4 gap-5">
-            <figure className=" rounded-2xl"><img className="rounded-2xl mx-auto " src={authorImage} alt="" /></figure>
+            <figure className=" rounded-2xl"><img className=" w-full rounded-2xl mx-auto " src={authorImage} alt="" /></figure>
             <div className=" col-span-3">
                 <h1 className=" font-bold text-xl">{author}</h1>
                 <p><span className=" font-bold text-lg">Bio:</span> {authorBio}</p>
